b812d1319c8d52893cf2f33b99aa8b1e7b32f611
gy <qldir116@gmail.com>
2022-10-24 17:50:13 +0900

    python function
    1) 함수 정의
    2) 내장/외장함수

---
+++ python_function.py
@@ -0,0 +1,38 @@
+# 1. 함수 정의 => def
+# def name(input):
+#     print("제 이름은 " + input + "입니다.")
+# name("가영")
+
+# def sum(a,b):
+#     return a+b
+# total = sum(3,4)
+# print(total)
+
+# Q. 성적과 이름을 입력하고, 성적에 따른 등급을 알려주는 프로그램을 만드시오.
+# grade = ''
+# def check_grade(성적):
+#     if 성적 >= 90:
+#         grade = 'A'
+#     elif 80 <= 성적 < 90:
+#        grade = 'B'
+#     elif 70 <= 성적 < 80:
+#         grade = 'C'
+#     elif 60 <= 성적 < 70:
+#        grade = 'D'
+#     else:
+#         grade = 'F'
+#     return grade
+# a = check_grade(90)
+# print(a)
+
+# 2. 내장함수 / 외장함수
+# 내장함수
+# range(), print()
+# a="python"
+# a=a.upper()
+# print(a) #PYTHON
+
+# 외장함수
+# import random
+# b=random.random()
+# print(b)
