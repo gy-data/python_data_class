26cf136db1f780e7278a81e3c0109da64a16a786
gy <qldir116@gmail.com>
2022-11-02 14:57:29 +0900

    python basic grammar

---
+++ 기초문법/variables_and_functions.py
@@ -0,0 +1,18 @@
+# 1. Variables
+my_age = 88  # snake case
+myAge = 88  # camel case
+# 88my_age = 88 <- 불가능
+# 변수의 형태 - 시작은 항상 문자
+a = 1
+b = 5
+c = a+b  # = 5
+c = 1  # = 1
+print(c)  # = 1 <= 파이썬은 위에서 아래로 읽기 때문
+
+# 2. Booleans and Strings - data types
+my_name = False  # bool -> True or False
+# my_age = "12" # str(문자열) -> 텍스트를 인식시켜주기 위해서 "" 사용
+# my_age =12 # int
+print(my_name)
+
+# 3.
